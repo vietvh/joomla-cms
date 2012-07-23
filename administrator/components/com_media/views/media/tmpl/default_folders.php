47a2f89d0137fbdb3361f2a3e5005a88bc494565
Michael Babker <mbabker@flbab.com>
2012-07-22 20:41:07 -0500

    Add isset check in case no children exist

--- administrator/components/com_media/views/media/tmpl/default_folders.php
+++ administrator/components/com_media/views/media/tmpl/default_folders.php
@@ -14,7 +14,8 @@ $ulTarget = str_replace('/', '-', $this->folders['data']->relative);
 
 ?>
 <ul class="nav nav-list collapse in" id="collapseFolder-<?php echo $ulTarget; ?>">
-<?php foreach ($this->folders['children'] as $folder) :
+<?php if (isset($this->folders['children'])) :
+	foreach ($this->folders['children'] as $folder) :
 	// Get a sanitised name for the target
 	$target = str_replace('/', '-', $folder['data']->relative); ?>
 	<li id="<?php echo $target; ?>">
@@ -24,5 +25,6 @@ $ulTarget = str_replace('/', '-', $this->folders['data']->relative);
 		</a>
 		<?php echo $this->getFolderLevel($folder); ?>
 	</li>
-<?php endforeach; ?>
+<?php endforeach;
+endif; ?>
 </ul>
